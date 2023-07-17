1f09a0cccdbbfceac60e4770d60a3a2fb91a7157
Isaac Korankye Asante <xquisit31@gmail.com>
2023-07-17 18:09:33 +0000

    learning java

---
+++ CmdLineAreaOfTriangle.java
@@ -0,0 +1,18 @@
+public class CmdLineAreaOfTriangle {
+    // Java codes always starts with a class
+    public static  void main(String[] args) {
+        // After the class you create a method most of the inputs are void cause you won't return anything
+        double height, base, area;
+        // Delcaration of variables
+        base = Double.parseDouble(args[0]);
+        /*Apparently when you do the code like this you input numbers as strings
+         * then you change it into a double or an integer
+         * According to lecturer this only works on Cmd Lines
+         */
+        height = Double.parseDouble(args[1]);
+        // Same goes for this line
+        area = 0.5 * height * base;
+        System.out.println("The area of the triangle = " + area);
+    } // that's the way we print in the town
+    
+}
