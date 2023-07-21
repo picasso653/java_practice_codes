20482cac47aa2b130d75d70291c4861e5f88437d
Isaac Korankye Asante <xquisit31@gmail.com>
2023-07-21 00:58:18 +0000

    wrong

--- DontPushNegative.java
+++ DontPushNegative.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class DontPushNegative {
     public static void main(String[] args) {
-        try (Scanner s = new Scanner(System.in)) {
-            System.out.println("Enter a positive number: ");
-            int k = s.nextInt();
-            if (k < 0){
-                System.out.println("Don't enter a negative number");
-            }
+        Scanner s = new Scanner(System.in); 
+            int k;
+            System.out.println("Enter a pistive number: ");
+            k = s.nextInt();
+            while (k != 0){
+                if (k < 1){
+                    break;
+                }
+            
         }
     }
 
